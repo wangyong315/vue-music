1128f7d82e954cc47c14212ed1e7c64d4a9f4646
wangyong <wangyong1935@126.com>
2021-07-01 23:02:13 +0800

    fix: 开发slider组件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot></slot>
+      <div class="dots"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+.slider
+  min-height: 1px
+  .slider-group
+    position: relative
+    overflow: hidden
+    white-space: nowrap
+    .slider-item
+      float: left
+      box-sizing: border-box
+      overflow: hidden
+      text-align: center
+      a
+        display: block
+        width: 100%
+        overflow: hidden
+        text-decoration: none
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="hello">
+    <slider></slider>
     recommend
   </div>
 </template>
 
 <script>
+import Slider from 'src/base/slider/slider'
+
 export default {
+  components: {
+    Slider
+  }
 }
 </script>
 
